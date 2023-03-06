7d6e0061293cb67e0ee929fe0e5c0254d542cfc8
Amath Sow <asow9276@gmail.com>
2023-03-06 12:50:15 +0000

    Add proper page titles

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+@section('title')
+Hello City
+@endsection
 @section('content')
     <h1>Bienvenue a Medina ndiobene</h1>
     <h2>HEURE DE Dakar:{{ date('h:i A') }}</h2>
--- resources/views/pages/home1.blade.php
+++ resources/views/pages/home1.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+@section('title')
+AMATH | Hello City
+@endsection
 @section('content')
     <p>Amath sow le debutant en developpement</p>
     <p><a href="/">Revenir a la page d'acceuil</a></p>
