5f84d6bb15172cb5f05b4f0db4f5bc2174aeebeb
Amath Sow <asow9276@gmail.com>
2023-03-06 11:19:44 +0000

    Remove duplication by using base layout

--- resources/views/pages/home1.blade.php
+++ resources/views/pages/home1.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>AMATH | Hello city</title>
-
-    </head>
-    <body>
-       <p>Amath sow le debutant en developpement</p>
-       <p><a href="/">Revenir a la page d'acceuil</a></p>
-        <footer>
-            <p>&copy:Copyright{{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@extends('base')
+@section('content')
+    <p>Amath sow le debutant en developpement</p>
+    <p><a href="/">Revenir a la page d'acceuil</a></p>
+@endsection
