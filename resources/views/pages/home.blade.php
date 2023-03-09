48af4ea1b211d59f74c2ef6e62cde0ee222b61be
Amath Sow <asow9276@gmail.com>
2023-03-09 17:50:55 +0000

    Add images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 Hello City
 @endsection
 @section('content')
+<img src="/images/senegal.png" alt="Drapeau Senegal">
     <h1>Bienvenue a Medina ndiobene</h1>
     <h2>HEURE DE Dakar:{{ date('h:i A') }}</h2>
 @endsection
