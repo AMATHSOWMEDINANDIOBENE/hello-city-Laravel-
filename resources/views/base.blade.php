a0f3e736d7ddc497f51f1d2c5f010246f3adaab0
Amath Sow <asow9276@gmail.com>
2023-03-09 17:25:18 +0000

    Do not display footer about us link on about page

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -58,7 +58,10 @@
 
     {{ config('projet.slogan')}}
     <footer>
-        <p>&copy:Copyright{{ date('Y') }} &middot; <a href="{{route('home1')}}">HOME1</a></p>
+        <p>&copy:Copyright{{ date('Y') }}
+        @if (! Route::is('home1'))
+            &middot; <a href="{{route('home1')}}">HOME1</a></p>
+        @endif
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
