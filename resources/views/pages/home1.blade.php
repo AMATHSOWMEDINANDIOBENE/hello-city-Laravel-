1d856ae49b6b550f438bb148f951bbf7e981243f
Amath Sow <asow9276@gmail.com>
2023-03-09 18:17:52 +0000

    Make use of asset function help to load images

--- resources/views/pages/home1.blade.php
+++ resources/views/pages/home1.blade.php
@@ -3,7 +3,7 @@
 AMATH | Hello City
 @endsection
 @section('content')
-<img src="/images/amath.png" alt="CV AMATH SOW">
+<img src="{{asset('/images/amath.png')}}" alt="CV AMATH SOW">
     <p>Amath sow le debutant en developpement</p>
     <p><a href="{{route('home')}}">Revenir a la page d'acceuil</a></p>
 @endsection
